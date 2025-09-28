a2ad92a2299bf8fa3b9c032279c06fcd23f443ab
Gvrdengrows <Puydie42@gmail.com>
2025-09-28 21:30:34 +0900

    asd

---
+++ javascript/node.md
@@ -0,0 +1,4 @@
+Это платформа скажем так где запускается javascript код
+
+Запуск кода:
+node javascript/variables.js
---
+++ javascript/variables.js
@@ -0,0 +1,9 @@
+// переменную конст нельзя перезначать
+const a = 5;
+// TypeError: Assignment to constant variable.
+a = 6;
+
+// переменную лет можно
+let b = 1;
+// Тут ошибки не будет
+b = 5;
